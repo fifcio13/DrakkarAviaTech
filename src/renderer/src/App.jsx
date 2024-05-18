218d61657d22d73dc6b06afd2e0fc9c240c2fd41
Filip Krawczyk <fkrawczyk@e-point.pl>
2024-05-18 18:07:16 +0200

    Implement loader

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,21 +1,44 @@
+import React, { useState } from 'react'
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps'
 import Versions from './components/Versions'
+import electronLogo from './assets/logo.png'
 
 function App() {
+  const [shouldShowLoader, setShouldShowLoader] = useState(true)
+  const applyGlobalStyle = () => {
+    setTimeout(() => {
+      const style = document.createElement('style')
+      style.innerHTML = `
+        .loader {
+          display: none;
+        }
+        #root {
+          display: block !important;
+        }
+      `
+      document.head.appendChild(style)
+    }, 4500)
+  }
+
   return (
     <>
-      <APIProvider apiKey={'AIzaSyC6683MhH9u4L1iAdQX6JM-axhi6yB6X3k'}>
+      {shouldShowLoader && (
+        <div className="loader fadeOut">
+          <img src={electronLogo} alt="Drakkar" style={{ maxWidth: '100vw', maxHeight: '100vh' }} />
+          <Versions />
+        </div>
+      )}
+      <APIProvider apiKey={'AIzaSyC6683MhH9u4L1iAdQX6JM-axhi6yB6X3k'} onLoad={applyGlobalStyle}>
         <Map
           style={{ width: '100vw', height: '100vh' }}
           defaultCenter={{ lat: 22.54992, lng: 0 }}
-          defaultZoom={3}
+          defaultZoom={5}
           gestureHandling={'greedy'}
           disableDefaultUI={true}
         >
           <Marker position={{ lat: 53.54992, lng: 10.00678 }} />
         </Map>
       </APIProvider>
-      <Versions></Versions>
     </>
   )
 }
--- src/renderer/src/components/Versions.jsx
+++ src/renderer/src/components/Versions.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
 function Versions() {
-  const [versions] = useState(window.electron.process.versions)
-
   return (
     <ul className="versions">
-      <li className="electron-version">Electronxx v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+      <li className="electron-version">Drakkar v1.0.0</li>
+      <li className="chrome-version">Made for AviaTech Hackathon</li>
     </ul>
   )
 }
